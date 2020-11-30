1d1853e70acf5b6bf6d6c2bf888951b7652773e4
egarciarey1981 <egarciarey1981@gmail.com>
2020-11-30 19:40:19 +0100

    Lista de funciones

--- 02. Introducción a Haskell/02_28_lista_de_funciones.hs
+++ 02. Introducción a Haskell/02_28_lista_de_funciones.hs
@@ -1,10 +1,8 @@
-{-
-Sea una lista de funciones de enteros en enteros:
-[f1, f2, ... , fn] :: [Integer -> Integer]
-
-Defina un operador (|>) de forma que se tenga:
-[f1, f2, ... , fn] |> x :: [f1 x, f2 x, ... , fn x] 
--}
+-- Sea una lista de funciones de enteros en enteros:
+-- [f1, f2, ... , fn] :: [Integer -> Integer]
+--
+-- Defina un operador (|>) de forma que se tenga:
+-- [f1, f2, ... , fn] |> x :: [f1 x, f2 x, ... , fn x] 
 
 (|>) :: [Integer -> Integer] -> Integer -> [Integer]
 [] |> _ = []
