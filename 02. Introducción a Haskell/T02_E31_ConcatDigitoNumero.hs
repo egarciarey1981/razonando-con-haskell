a259de4302e65249653b695603d4dbcc76fbbf81
egarciarey1981 <egarciarey1981@gmail.com>
2020-12-01 18:01:54 +0100

    Dígito a la derecha de un entero

---
+++ 02. Introducción a Haskell/T02_E31_ConcatDigitoNumero.hs
@@ -0,0 +1,6 @@
+-- Escriba una función que añada un dígito a la derecha de un número entero:
+-- MAIN> 3 `aLaDerechaDe` 146
+-- 1463 :: Integer
+
+aLaDerecha :: Integer -> Integer -> Integer
+aLaDerecha digito entero = entero * 10 + digito
