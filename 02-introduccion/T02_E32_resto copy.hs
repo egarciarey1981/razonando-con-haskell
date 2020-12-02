1d8c90fbcf55c15b256bdd8955534a07ae4ac051
egarciarey1981 <egarciarey1981@gmail.com>
2020-12-02 18:41:13 +0100

    Resto de una división

---
+++ 02-introduccion/T02_E32_resto copy.hs
@@ -0,0 +1,7 @@
+-- Escriba una función recursiva que devuelva el resto de la división de dos
+-- enteros usando sustracciones.
+
+resto :: Integer -> Integer -> Integer
+resto x y
+  | x < y = x
+  | otherwise = resto (x - y) y
